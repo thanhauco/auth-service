0a8911ced9c6953043e4a22385d9d7137c74b5f5
Thanh Vu <thanhauco@gmail.com>
2021-01-22 01:35:00 -0800

    Implement Login

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,8 +1,15 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const SECRET = 'secret';
 module.exports = {
   register: async (email, password) => {
     const hash = await bcrypt.hash(password, 10);
     User.create({ email, password: hash });
+  },
+  login: async (email, password) => {
+    const user = User.find(email);
+    if (!user || !await bcrypt.compare(password, user.password)) throw new Error('Invalid credentials');
+    return jwt.sign({ email }, SECRET, { expiresIn: '1h' });
   }
 };
