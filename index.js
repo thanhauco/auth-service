aa1cee7fd8c6e906f310351b75f68581fa197cae
Thanh Vu <thanhauco@gmail.com>
2021-01-24 19:54:00 -0800

    Add Register Endpoint

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const auth = require('./src/services/auth');
 const app = express();
-app.get('/', (req, res) => res.send('Auth Service'));
+app.use(express.json());
+app.post('/register', async (req, res) => {
+  try {
+    await auth.register(req.body.email, req.body.password);
+    res.status(201).send('Created');
+  } catch (e) { res.status(400).send(e.message); }
+});
 app.listen(3000, () => console.log('Listening on 3000'));
