a5dc5f64337aecd306f79436e78a76143a8255e7
Thanh Vu <thanhauco@gmail.com>
2021-01-30 02:45:00 -0800

    Add Refresh Endpoint

--- index.js
+++ index.js
@@ -3,17 +3,11 @@ const auth = require('./src/services/auth');
 const authMiddleware = require('./src/middleware/auth');
 const app = express();
 app.use(express.json());
-app.post('/register', async (req, res) => {
+// ... existing routes
+app.post('/token', (req, res) => {
   try {
-    await auth.register(req.body.email, req.body.password);
-    res.status(201).send('Created');
-  } catch (e) { res.status(400).send(e.message); }
-});
-app.post('/login', async (req, res) => {
-  try {
-    const token = await auth.login(req.body.email, req.body.password);
+    const token = auth.refresh(req.body.token);
     res.json({ token });
-  } catch (e) { res.status(401).send(e.message); }
+  } catch (e) { res.sendStatus(403); }
 });
-app.get('/profile', authMiddleware, (req, res) => res.json(req.user));
 app.listen(3000, () => console.log('Listening on 3000'));
