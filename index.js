aacddef1b6a0437bf3b756c9a2de6a7daab8d020
Thanh Vu <thanhauco@gmail.com>
2021-02-14 16:49:00 -0800

    Configure Rate Limit

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const auth = require('./src/services/auth');
 const authMiddleware = require('./src/middleware/auth');
 const validate = require('./src/middleware/validate');
 const app = express();
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
+app.use(limiter);
 app.use(express.json());
-app.post('/register', validate, async (req, res) => {
-  try {
-    await auth.register(req.body.email, req.body.password);
-    res.status(201).send('Created');
-  } catch (e) { res.status(400).send(e.message); }
-});
 // ... rest
