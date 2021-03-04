d957592c56dc771fabfe16046398a8fbe299a0ac
Thanh Vu <thanhauco@gmail.com>
2021-03-04 02:41:00 -0800

    Implement 2FA Verify

--- src/services/mfa.js
+++ src/services/mfa.js
@@ -1,9 +1,6 @@
 const speakeasy = require('speakeasy');
-const qrcode = require('qrcode');
 module.exports = {
-  setup: async () => {
-    const secret = speakeasy.generateSecret();
-    const url = await qrcode.toDataURL(secret.otpauth_url);
-    return { secret: secret.base32, url };
+  verify: (secret, token) => {
+    return speakeasy.totp.verify({ secret, encoding: 'base32', token });
   }
 };
