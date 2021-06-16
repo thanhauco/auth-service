6a9c8c9423e8b22b481b7a06cf7811607bbf2e24
Thanh Vu <thanhauco@gmail.com>
2021-06-16 07:40:00 -0700

    Add Swagger UI

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-// Fixed lint errors
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
