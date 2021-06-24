8e24ec2d0d617e4032c5a8a6782d81849e917613
Thanh Vu <thanhauco@gmail.com>
2021-06-23 17:12:00 -0700

    Add Morgan

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const morgan = require('morgan');
+app.use(morgan('combined'));
